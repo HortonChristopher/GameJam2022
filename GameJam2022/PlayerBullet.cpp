2e29efe944ddf99edbee4e02149d47d8c857d430
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2022-09-13 17:52:03 +0900

    bulletのスピード更新

--- GameJam2022/PlayerBullet.cpp
+++ GameJam2022/PlayerBullet.cpp
@@ -295,9 +295,9 @@ void PlayerBullet::Update()
 
 	//更新処理
 	//position.x += 3.0f;
-	position.x += velocity_.m128_f32[0] / 20.0f; // 弾丸を速くしたり
-	position.y += velocity_.m128_f32[1] / 20.0f; // 遅くしたい場合は、
-	position.z += velocity_.m128_f32[2] / 20.0f; // 10.0f を変更します。
+	position.x += velocity_.m128_f32[0] / 10.0f; // 弾丸を速くしたり
+	position.y += velocity_.m128_f32[1] / 10.0f; // 遅くしたい場合は、
+	position.z += velocity_.m128_f32[2] / 10.0f; // 10.0f を変更します。
 
 	// 当たり判定更新 Collision detection update
 	if (collider) {
